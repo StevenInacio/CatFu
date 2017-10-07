f01f103154403e5dccb27a3ad61a174c7303e4ff
Steven Inácio <spikevalantine@hotmail.de>
2017-10-07 10:01:16 +0200

    First commit
    Initial Commit to start the project.
    Name ist still a WIP.

---
+++ src/main/scala/de/htwg/se/CathulhusMaze/CathulhusMaze.scala
@@ -0,0 +1,14 @@
+package de.htwg.se.CathulhusMaze
+
+import de.htwg.se.CathulhusMaze.model.Player
+
+object CathulhusMaze {
+  def main(args: Array[String]): Unit = {
+    val student = Player("Your Name")
+    println("Hello, " + student.name)
+  }
+}
+
+object MainMenu() {
+  
+}
