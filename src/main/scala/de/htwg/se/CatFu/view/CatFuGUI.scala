f7228535d4a597bfb28923fbbafc02753e9c9c39
sammyCatlady42 <sammy_isted@t-online.de>
2018-01-24 20:15:45 +0100

    fixed Gui I guess

--- src/main/scala/de/htwg/se/CatFu/view/CatFuGUI.scala
+++ src/main/scala/de/htwg/se/CatFu/view/CatFuGUI.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.CatFu.view
 
-import java.awt._
+import java.awt.Color
 
 import de.htwg.se.CatFu.logic.PlayerManagement
 import de.htwg.se.CatFu.model._
@@ -9,14 +9,12 @@ import scala.swing._
 
 //noinspection ScalaStyle
 class CatFuGUI extends Frame {
-  val p: List[Player] = PlayerManagement.randomTeam()
-  val e: List[Player] = PlayerManagement.enemyTeam(p)
-
 
   val board = new Field()
+  val p: List[Player] = PlayerManagement.randomTeam()
+  val e: List[Player] = PlayerManagement.enemyTeam(p)
   board.clearField()
   board.setRandomObstacle()
-  val butt = new Button()
   board.setUpTeams(p, e)
 
 
@@ -25,44 +23,43 @@ class CatFuGUI extends Frame {
   title = "CatFu"
 
   contents = new BorderPanel {
-    //add(new Button("North"), BorderPanel.Position.North)
-    //add(new Button("Center"), BorderPanel.Position.Center)
-    //add(new Button("East"), BorderPanel.Position.East)
-    //add(new Button("West"), BorderPanel.Position.West)
     add(Button("South: Close") {
       sys.exit(0)
     }, BorderPanel.Position.South)
-
 
     add(c = new GridPanel(13, 13) {
       var block = true
       contents += new Label("     ")
       for (i <- 0 until 12) {
-          for (k <- 0 until board.xfield) {
-          if(block){contents += new Label("  " + k + "  ")
+        for (k <- 0 until board.xfield) {
+          if (block) {
+            contents += new Label("  " + k + "  ")
           }
           block = false
 
-        contents += new Label("  " + i + "  ")
-        for (j <- 0 until board.yfield) {
-          foreground = Color.blue
-          background = Color.green
-
-          butt.background = Color.red
-          //contents += butt
-          contents += new Button() {
-            background = Color.orange
-            foreground = Color.black
-            val figure: String = board.getDisplay(i, j).toString
-            text = figure
+          contents += new Label("  " + i + "  ")
+          for (j <- 0 until board.yfield) {
+            foreground = Color.blue
+            background = Color.green
+            var butt = new Button()
+            butt.background = Color.red
+            //contents += butt
+            contents += new Button() {
+              background = Color.orange
+              foreground = Color.black
+              val figure: String = board.getDisplay(i, j).toString
+              text = figure
+            }
+            //i ist x ist zeile
+            //j = y = spalte
           }
-          //i ist x ist zeile
-          //j = y = spalte
         }
       }
       repaint()
 
-    }, l = BorderPanel.Position.Center)
-  }
 
+    }, l = BorderPanel.Position.Center)
+
+
+  }
 }
